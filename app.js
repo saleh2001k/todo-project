b9f2118f7cb3642e215aa247db42cb57eb1f6e84
saleh2001k <saleh.ayman2001@gmail.com>
2023-04-11 22:21:01 +0300

    adding a yes or no question system

--- app.js
+++ app.js
@@ -32,3 +32,41 @@ if (!welcomeMessage) {
       alert('Welcome,'+ User );
     }
 }
+
+
+
+
+
+  
+const questions = [
+    'Do you like java script? (yes/no)',
+    'Do you like node.jd? (yes/no)',
+    'Last but not least, Do you like pizza? (yes/no)'
+  ]
+
+
+function askQuestion() {
+
+    const answers = [];
+  
+    for (let i = 0; i < questions.length; i++) {
+      let input = prompt(questions[i]);
+
+      if (input !== 'yes'&& input !=='no'){
+        answers.push('invaled answare')
+      }
+      else{
+
+      answers.push(input);
+        }
+    }
+  
+    return answers;
+  }
+  
+  const answers = askQuestion();
+  
+  console.log(answers);
+  
+  
+  
